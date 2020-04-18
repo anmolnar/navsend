d9691f529a2da88ec2571da40e5e0dcc7521eee1
Andor Molnar <andor@cloudera.com>
2020-04-18 23:38:55 +0200

    Removed NavStatus table

--- sql/llx_navsend_navstatus.key.sql
+++
@@ -1,5 +0,0 @@
--- Copyright (C) 2020 Andor Molnár <andor@apache.org>
-
-ALTER TABLE llx_navsend_navstatus ADD INDEX idx_navsend_navstatus_rowid (rowid);
-ALTER TABLE llx_navsend_navstatus ADD INDEX idx_navsend_navstatus_ref (ref);
-ALTER TABLE llx_navsend_navstatus ADD CONSTRAINT llx_navsend_navstatus_fk_invoice FOREIGN KEY (fk_invoice) REFERENCES llx_facture(rowid);
