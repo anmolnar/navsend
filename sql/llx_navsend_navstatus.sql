28c54f59a9cef1532842ea3cdbda8ce4c02d8ca4
Andor Molnar <andor@cloudera.com>
2020-04-18 22:13:10 +0200

    Enable modulebuilder, finalize data structure

--- sql/llx_navsend_navstatus.sql
+++ sql/llx_navsend_navstatus.sql
@@ -1,13 +1,10 @@
 -- Copyright (C) 2020 Andor Molnár <andor@apache.org>
 
-CREATE TABLE llx_natsend_natstatus(
+CREATE TABLE llx_navsend_navstatus(
 	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	ref varchar(128) NOT NULL, 
-	description text, 
-	date_creation datetime NOT NULL, 
-	tms timestamp, 
-	fk_user_creat integer NOT NULL, 
-	fk_user_modif integer, 
-	fk_invoice integer NOT NULL, 
-	status smallint NOT NULL
+	fk_invoice integer NOT NULL,
+	errcode smallint NOT NULL,
+	msg varchar(255),
+    ts timestamp
 ) ENGINE=innodb;
