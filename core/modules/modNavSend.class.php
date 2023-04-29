35e3683f88f2c616b86beb1b0e552c335127b190
Andor Molnar <andor@apache.org>
2023-04-29 14:56:59 +0200

    Bump version: 1.1.6

--- core/modules/modNavSend.class.php
+++ core/modules/modNavSend.class.php
@@ -36,7 +36,7 @@ class modNavSend extends DolibarrModules
 		$this->editor_name = 'Andor Molnar';
 		$this->editor_url = 'mailto:andor@apache.org';
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.1.5';
+		$this->version = '1.1.6';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
